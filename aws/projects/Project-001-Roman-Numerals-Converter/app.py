ffdd75b5428d6eb36656cc4930368466aa0bf847
MehmetKulahci <mhmtklhci@gmail.com>
2022-10-17 09:59:17 +0300

    503 eklendi

--- aws/projects/Project-001-Roman-Numerals-Converter/app.py
+++ aws/projects/Project-001-Roman-Numerals-Converter/app.py
@@ -26,5 +26,5 @@ def main_post():
         return render_template('index.html', developer_name='mehmet', not_valid=False)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=80)
